98955fc6e730517acb8ec669701624b21a96fe67
Bruno <brunohgrau99@gmail.com>
2024-04-02 02:33:45 -0300

    redux-setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+import { Provider } from "react-redux";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
+import store from "./store";
 
 const router = createBrowserRouter(
   createRoutesFromElements(<Route path="/" element={<App />}></Route>)
@@ -21,7 +22,9 @@ async function deferRender() {
 deferRender().then(() => {
   ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </React.StrictMode>
   );
 });
---
+++ src/slices/apiSlice.js
@@ -0,0 +1,72 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseQuery = fetchBaseQuery({ baseUrl: "/fakeApi" });
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery,
+  tagTypes: ["Post"],
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () => "/posts",
+      providesTags: (result = [], error, arg) => [
+        "Post",
+        ...result.map(({ id }) => ({ type: "Post", id })),
+      ],
+    }),
+    getPost: builder.query({
+      query: (postId) => `/posts/${postId}`,
+      providesTags: (result, error, arg) => [{ type: "Post", id: arg }],
+    }),
+    addNewPost: builder.mutation({
+      query: (initialPost) => ({
+        url: "/posts",
+        method: "POST",
+        // Include the entire post object as the body of the request
+        body: initialPost,
+      }),
+      invalidatesTags: ["Post"],
+    }),
+    editPost: builder.mutation({
+      query: (post) => ({
+        url: `posts/${post.id}`,
+        method: "PATCH",
+        body: post,
+      }),
+      invalidatesTags: (result, error, arg) => [{ type: "Post", id: arg.id }],
+    }),
+
+    deletePost: builder.mutation({
+      query({ id }) {
+        return {
+          url: `posts/${id}`,
+          method: "DELETE",
+          body: { id },
+        };
+      },
+      invalidatesTags: (result, error, arg) => [{ type: "Post", id: arg.id }],
+    }),
+
+    addReaction: builder.mutation({
+      query: ({ postId, reaction }) => ({
+        url: `posts/${postId}/reactions`,
+        method: "POST",
+        // In a real app, we'd probably need to base this on user ID somehow
+        // so that a user can't do the same reaction more than once
+        body: { reaction },
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: "Post", id: arg.postId },
+      ],
+    }),
+  }),
+});
+
+export const {
+  useGetPostsQuery,
+  useGetPostQuery,
+  useAddNewPostMutation,
+  useEditPostMutation,
+  useAddReactionMutation,
+  useDeletePostMutation,
+} = apiSlice;
